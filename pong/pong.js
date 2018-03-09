855bfbc6f988a5ab5d6b6700dee243a80cb63f65
Olly Shaw <ollyjshaw@gmail.com>
2018-03-09 21:43:07 +0000

    moved to node

--- pong/pong.js
+++ pong/pong.js
@@ -1,4 +1,11 @@
-vertx.createHttpServer().requestHandler(function (request) {
-    console.log("<< PONG!")
-    request.response().end("<h1>PONG!!!</H1>");
-}).listen(8080);
+const connect = require('connect')
+const http = require('http')
+
+const app = connect()
+
+app.use( (req, res) => {
+  console.log('<< PONG!!')
+  res.end('<h1>Hello from ponger</h1>')
+})
+
+http.createServer(app).listen(8080)
